ee7896651938fc721d8cf9be05d40510b00a6c7f
obiwan <ob.kwon@dreamus.io>
2024-12-04 10:49:24 +0900

    .

--- _entrypoint.sh
+++ _entrypoint.sh
@@ -4,15 +4,19 @@ set -e
 CUR_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$CUR_DIR"
 
-function train_prod () {
-    accelerate launch --config_file accelerate_prod.yaml \
+function train_multi () {
+    accelerate launch --config_file accelerate_multi.yaml \
     src/main.py -t train -c res/config.train.yml
 }
 
+function train_single () {
+    accelerate launch --config_file accelerate_single.yaml \
+    src/main.py -t train -c res/config.train.yml
+}
 
 if [[ "$1" = "train" ]]; then
     shift 1
-    train_prod
+    train_single
 else
     eval "$@"
 fi
